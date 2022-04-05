bfc9f79f3c542072bb40fd27698edde59d801f49
Anton_Yamkin <yamkin73@gmail.com>
2022-04-05 21:29:12 +0300

    develop pagination component and added check propTypes

--- src/app/App.js
+++ src/app/App.js
@@ -1,27 +1,35 @@
-import React, { useState } from 'react';
-import Users from './components/users';
-import SearchStatus from './components/searchStatus';
-import api from './API';
+import React, { useState } from "react";
+import Users from "./components/users";
+import SearchStatus from "./components/searchStatus";
+import api from "./API";
 
 function App() {
-  const [users, setUsers] = useState(api.users.fetchAll());
-  const handleDelete = (userId) => {
-    setUsers(users.filter((user) => user._id !== userId));
-  };
+    const [users, setUsers] = useState(api.users.fetchAll());
+    const handleDelete = (userId) => {
+        setUsers(users.filter((user) => user._id !== userId));
+    };
 
-  const handleToggleBookMark = (id) => {
-    const userIndex = users.findIndex((user) => user._id === id);
-    const newUsers = [...users];
-    newUsers[userIndex].bookmark = !newUsers[userIndex].bookmark;
-    setUsers(newUsers);
-  };
+    const handleToggleBookMark = (id) => {
+        setUsers(
+            users.map((user) => {
+                if (user._id === id) {
+                    return { ...user, bookmark: !user.bookmark };
+                }
+                return user;
+            })
+        );
+    };
 
-  return (
-    <div>
-      <SearchStatus length={users.length} />
-      <Users users={users} onDelete={handleDelete} onToggleBookMark={handleToggleBookMark} />
-    </div>
-  );
+    return (
+        <div>
+            <SearchStatus length={users.length} />
+            <Users
+                users={users}
+                onDelete={handleDelete}
+                onToggleBookMark={handleToggleBookMark}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 const BookMark = ({ status, ...rest }) => {
-  return (
-    <button {...rest}>
-      <i className={'bi bi-bookmark' + (status === false ? '' : '-fill')}></i>
-    </button>
-  );
+    console.log(status);
+    return (
+        <button {...rest}>
+            <i
+                className={"bi bi-bookmark" + (status === false ? "" : "-fill")}
+            ></i>
+        </button>
+    );
+};
+
+BookMark.propTypes = {
+    status: PropTypes.bool.isRequired
 };
 
 export default BookMark;
