a70c04500998ccdb9974c2a7d22da0b3e39c0b69
Anton_Yamkin <yamkin73@gmail.com>
2022-05-17 18:19:21 +0300

    adjusting indents throughout the project

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -5,33 +5,33 @@ import Qualities from "../../ui/qualities";
 import { useHistory, useParams } from "react-router-dom";
 
 const UserPage = () => {
-  const { userId } = useParams();
-  const history = useHistory();
-  const [user, setUser] = useState();
-  useEffect(() => {
-    api.users.getById(userId).then((data) => setUser(data));
-  }, []);
-  const handleClick = () => {
-    history.push(`/users/${userId}/edit`);
-  };
-  if (user) {
-    return (
-      <div className="m-4">
-        <h1> {user.name}</h1>
-        <h2>Профессия: {user.profession.name}</h2>
-        <Qualities qualities={user.qualities} />
-        <p>completedMeetings: {user.completedMeetings}</p>
-        <h2>Rate: {user.rate}</h2>
-        <button onClick={handleClick}> Изменить </button>
-      </div>
-    );
-  } else {
-    return <h1>Loading...</h1>;
-  }
+    const { userId } = useParams();
+    const history = useHistory();
+    const [user, setUser] = useState();
+    useEffect(() => {
+        api.users.getById(userId).then((data) => setUser(data));
+    }, []);
+    const handleClick = () => {
+        history.push(`/users/${userId}/edit`);
+    };
+    if (user) {
+        return (
+            <div className="m-4">
+                <h1> {user.name}</h1>
+                <h2>Профессия: {user.profession.name}</h2>
+                <Qualities qualities={user.qualities} />
+                <p>completedMeetings: {user.completedMeetings}</p>
+                <h2>Rate: {user.rate}</h2>
+                <button onClick={handleClick}> Изменить </button>
+            </div>
+        );
+    } else {
+        return <h1>Loading...</h1>;
+    }
 };
 
 UserPage.propTypes = {
-  userId: PropTypes.string.isRequired
+    userId: PropTypes.string.isRequired
 };
 
 export default UserPage;
--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -5,11 +5,19 @@ import UserPage from "../components/page/userPage";
 import UserEditForm from "../components/page/userEditForm";
 
 const Users = () => {
-  const params = useParams();
-  const { userId, edit } = params;
-  return (
-    <>{edit ? <UserEditForm /> : userId ? <UserPage /> : <UsersListPage />}</>
-  );
+    const params = useParams();
+    const { userId, edit } = params;
+    return (
+        <>
+            {edit ? (
+                <UserEditForm />
+            ) : userId ? (
+                <UserPage />
+            ) : (
+                <UsersListPage />
+            )}
+        </>
+    );
 };
 
 export default Users;
